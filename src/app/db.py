19604e6d58631b9d78dc536f8f06d8ea010d43d3
Psyhackological <konradkon@duck.com>
2025-01-02 21:59:22 +0100

    change from sqlite to postgresql

--- src/app/db.py
+++ src/app/db.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
+from .db_env import DATABASE_URL
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
